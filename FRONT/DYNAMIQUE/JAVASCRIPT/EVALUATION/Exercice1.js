e04d3804ce32f594a698bfa9c80a32d3514ce9ff
Delacroix Julie <julie.delacroix.e@gmail.com>
2020-09-17 15:21:56 +0200

    modif exo 1 eval js

--- FRONT/DYNAMIQUE/JAVASCRIPT/EVALUATION/Exercice1.js
+++ FRONT/DYNAMIQUE/JAVASCRIPT/EVALUATION/Exercice1.js
@@ -1,19 +1,21 @@
-var age = parseInt(window.prompt("Entrez un âge : "));
 var jeune = 0;
 var moyen = 0;
 var vieux = 0;
-
-while (age <= 100) {
-    age = parseInt(window.prompt("Entrez un âge : "));
+do {
+  var age = parseInt(window.prompt("Entrez un âge : "));
+    
 if (age < 20) {
     jeune++;
 }
-else if (age > 40) {
+else if (age > 40 ) {
     vieux++;
+  if (age == 100) {
+    break;
+  }
 }
 else {
     moyen++;
 }
-}
+} while (age <= 100);
 
 console.log(`nombre de jeune : ${jeune}, nombre de moyen : ${moyen}, nombre de vieux : ${vieux}`);
