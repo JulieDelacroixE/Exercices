42630945cdcfd34eb0fee9614875effad63bf768
Delacroix Julie <julie.delacroix.e@gmail.com>
2020-09-18 12:21:29 +0200

    ajout PDO

---
+++ BACK/SERVEUR/PHP/PDO/testat4/Liste.php
@@ -0,0 +1,54 @@
+<?php include("header.php");?>
+
+
+<?php
+    require "connexion_bdd.php"; // Inclusion de notre bibliothèque de fonctions
+    $db = connexionBase(); // Appel de la fonction de connexion
+    $requete = "SELECT pro_id, pro_ref, pro_libelle, pro_couleur, pro_prix, pro_photo FROM produits WHERE ISNULL(pro_bloque) ORDER BY pro_id ASC";
+
+    $result = $db->query($requete);
+
+    if (!$result) 
+    {
+        $tableauErreurs = $db->errorInfo();
+        echo $tableauErreur[2]; 
+        die("Erreur dans la requête");
+    }
+
+    if ($result->rowCount() == 0) 
+    {
+       // Pas d'enregistrement
+       die("La table est vide");
+    }
+
+    echo '<table class="table table-striped table-bordered m-auto">';
+    echo '<thead>
+    <tr class="table-active h4">
+        <th scope="col">Photo</th>
+        <th scope="col">ID</th>
+        <th scope="col">Catégorie</th>
+        <th scope="col">Libellé</th>
+        <th scope="col">Prix</th>
+        <th scope="col">Couleur</th>
+    </tr>
+</thead>';
+
+    while ($row = $result->fetch(PDO::FETCH_OBJ))
+    {
+        echo '<tr class="table-warning">';
+        echo '<td><img class ="img-fluid" src= "jarditou_photos/'.$row->pro_id.'.'.$row->pro_photo.'"alt='.$row->pro_libelle.'></td>';
+        echo '<td>'.$row->pro_id.'</td>';
+        echo '<td>'.$row->pro_ref.'</td>';
+        echo '<td><a href="detail.php?id='.$row->pro_id.'" title='.$row->pro_libelle.'>'.$row->pro_libelle.'</a></td>';
+        echo '<td>'.$row->pro_prix.'</td>';
+        echo '<td>'.$row->pro_couleur.'</td>';
+        echo"</tr>";
+    }
+
+    echo "</table>"; 
+    ?>
+
+
+<?php 
+    include("footer.php");
+?>
---
+++ BACK/SERVEUR/PHP/PDO/testat4/header.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <title>Jarditou accueil</title>
+</head>
+<body>
+    <div class="container">
+        <header>
+            <div class="row justify-content-between align-self-start">
+                <div class="col-4">
+                    <img class="img-fluid" src="public/images/jarditou_logo.jpg" alt="jarditoulogo" width="200" height="auto">
+                </div>
+                <div class="col-4">
+                    <p class="h2 ml-5 pt-3 d-none d-lg-block">Tout le jardin</p>
+                </div>
+            </div>
+
+    <nav class="navbar navbar-expand-sm bg-light navbar-light">
+        <a class="navbar-brand" href="#">Jarditou.com</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+        </button>   
+        <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="Index.php">Accueil</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="Tableau.php">Tableau</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="formulaire.php">Formulaire</a>
+                </li>
+            </ul>
+        </div>
+        <form class="form-inline d-none d-lg-block">
+            <input class="form-control mr-sm-2" type="search" placeholder="Votre promotion" aria-label="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Recherche</button>
+        </form>
+    </nav>
